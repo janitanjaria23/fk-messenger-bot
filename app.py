a824777d695c2c600814dc82e4af92af78239718
Janit Anjaria <janit92@gmail.com>
2017-04-06 17:36:15 +0530

    app.py request handler changes

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+import sys
+import json
+
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
-    return "Hello World!!!"
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
 
 
 if __name__ == "__main__":
