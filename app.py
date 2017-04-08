050aeea02dd938aefee04ec732081f53986cdddc
Janit Anjaria <janit92@gmail.com>
2017-04-08 12:07:11 +0530

    typcasting lat and lng values to str and then joining them

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def get_google_geocoding_api_response(address):
         resp = r.json()
         log(resp)
         if 'results' in resp and len(resp['results']) > 0:
-            location = ','.join([resp['results'][0]['geometry']['location']['lat'], resp['results'][0]['geometry']['location']['lng']])
+            location = ','.join([str(resp['results'][0]['geometry']['location']['lat']), str(resp['results'][0]['geometry']['location']['lng'])])
     except Exception as err:
         log(err)
 
